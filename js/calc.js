f8384e6769c84cc2d7e8c14cf66278253b64b81a
Rustam Abdulatipov <rabdulatipoff@gmail.com>
2017-08-07 01:29:16 +0300

    Added Liquid and Result panels

--- js/calc.js
+++ js/calc.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
+	// Tabs
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.calc-form').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+	// Calc sliders
+	// Nicotine base sliders
 	$("#vgBaseSlider").ionRangeSlider({
 		type: "single",
 		min: 0,
 		max: 100,
 		step: 1,
 		postfix: "%",
-		from: 50,
+		from: 0,
 
 		onStart: function(data) {
 			$("#vgBase").val(data.from);
@@ -18,7 +31,7 @@ $(document).ready(function() {
 		max: 100,
 		step: 1,
 		postfix: "%",
-		from: 50,
+		from: 100,
 
 		onStart: function(data) {
 			$("#pgBase").val(data.from);
@@ -50,6 +63,99 @@ $(document).ready(function() {
 			$("#nicBase").val(data.from);
 		}
 	});
+
+	// Juice sliders
+	$("#juiceVolumeSlider").ionRangeSlider({
+		type: "single",
+		min: 0,
+		max: 1000,
+		step: 1,
+		postfix: "мл",
+		from: 30,
+
+		onStart: function(data) {
+			$("#juiceVolume").val(data.from);
+		}
+	});
+	
+	$("#vgJuiceSlider").ionRangeSlider({
+		type: "single",
+		min: 0,
+		max: 100,
+		step: 1,
+		postfix: "%",
+		from: 50,
+
+		onStart: function(data) {
+			$("#vgJuice").val(data.from);
+		}
+	});
+
+	$("#pgJuiceSlider").ionRangeSlider({
+		type: "single",
+		min: 0,
+		max: 100,
+		step: 1,
+		postfix: "%",
+		from: 50,
+
+		onStart: function(data) {
+			$("#pgJuice").val(data.from);
+		}
+	});
+
+	$("#diluentJuiceSlider").ionRangeSlider({
+		type: "single",
+		min: 0,
+		max: 100,
+		step: 1,
+		postfix: "%",
+		from: 0,
+
+		onStart: function(data) {
+			$("#diluentJuice").val(data.from);
+		}
+	});
+
+	$("#pgFlavorsSlider").ionRangeSlider({
+		type: "single",
+		min: 0,
+		max: 100,
+		step: 1,
+		postfix: "%",
+		from: 5,
+
+		onStart: function(data) {
+			$("#pgFlavors").val(data.from);
+		}
+	});
+
+	$("#vgFlavorsSlider").ionRangeSlider({
+		type: "single",
+		min: 0,
+		max: 100,
+		step: 1,
+		postfix: "%",
+		from: 0,
+
+		onStart: function(data) {
+			$("#vgFlavors").val(data.from);
+		}
+	});
+
+	$("#nicJuiceSlider").ionRangeSlider({
+		type: "single",
+		min: 0,
+		max: 36,
+		step: 1,
+		postfix: " мг/мл",
+		from: 3,
+
+		onStart: function(data) {
+			$("#nicJuice").val(data.from);
+		}
+	});
+
 	
 	var $vgBaseSlider = $("#vgBaseSlider").data("ionRangeSlider");
 	var $pgBaseSlider = $("#pgBaseSlider").data("ionRangeSlider");
@@ -59,17 +165,28 @@ $(document).ready(function() {
 	var vgBaseValue = $("#vgBaseSlider").data("from");
 	var pgBaseValue = $("#pgBaseSlider").data("from");
 	var diluentBaseValue = $("#diluentBaseSlider").data("from");
-	
-	// Sync PG/VG sliders and their inputs
-	function recalcPgVg(component, value) {
-		var from, to;
-		switch(component) {
-			case "vg": {
-				//from = 
-			}
-		}
+	var nicBaseValue = $("#nicBaseSlider").data("from");
+
+	var juiceVolume = $("#juiceVolumeSlider").data("from");
+
+	var vgJuiceValue = $("#vgJuiceSlider").data("from");
+	var pgJuiceValue = $("#pgJuiceSlider").data("from");
+	var diluentJuiceValue = $("#diluentJuiceSlider").data("from");
+	var pgFlavorsValue = $("pgFlavorsSlider").data("from");
+	var vgFlavorsValue = $("#vgFlavorsSlider").data("from");
+	var nicJuiceStrength = $("#nicJuiceSlider").data("from");
+
+	$("#baseVolume").html(100);
+
+	function calcLiquid() {
+		vgJuiceVolume = ((vgBaseValue - vgFlavorsVolume) / 100.0) * volume;
+		pgJuiceVolume = ((pgBaseValue - pgFlavorsVolume) / 100.0) * volume;
+		pgFlavorsVolume = (pgFlavorsValue / 100.0) * volume;
+		vgFlavorsVolume = (vgFlavorsValue / 100.0) * volume;
+		diluentJuiceVolume = (diluentBaseSlider / 100.0) * volume;
 	}
 
+	// Sync PG/VG sliders and their inputs
 	$vgBaseSlider.update({
 		onChange: function(data) {
 			$("#vgBase").val(data.from);
@@ -79,8 +196,6 @@ $(document).ready(function() {
 
 			$pgBaseSlider.update({from: pgBaseValue});
 			$("#pgBase").val(pgBaseValue);
-			//console.log("vg onChange");
-			//
 		}
 	});
 	
@@ -123,31 +238,39 @@ $(document).ready(function() {
 			$vgBaseSlider.update({from: vgBaseValue, max: residue});
 			$pgBaseSlider.update({from: pgBaseValue, max: residue});
 			$("#vgBase").val(vgBaseValue);
+			$("#vgBase").attr("max", residue);
 			$("#pgBase").val(pgBaseValue);
+			$("#pgBase").attr("max", residue);
+
 			diluentBaseValue = data.from;
-			
-			if((vgBaseValue + pgBaseValue + diluentBaseValue) != 100) {
-				console.log("Total percentage >100!");
-			}
 		}
 	});
 
 	$nicBaseSlider.update({
 		onChange: function(data) {
 			$("#nicBase").val(data.from);
-		
+			nicBaseValue = data.from;
 		}
 	});
 
 	// Watch if any of the inputs were changed
 	$("#vgBase").on('keyup change', function() {
+		var residue = 100 - diluentBaseValue;
+		if($(this).val() < 0) {
+			vgBaseValue = 0;
+		} else if($(this).val() > residue) {
+			vgBaseValue = residue;
+		} else {
+			vgBaseValue = $(this).val();
+		}
+
 		$vgBaseSlider.update({
-			from: $(this).val()
+			from: vgBaseValue
 		});
 		$pgBaseSlider.update({
-			from: 100 - $(this).val()
+			from: residue - vgBaseValue
 		});
-		$("#pgBase").val(100 - $(this).val());
+		$("#pgBase").val(residue - vgBaseValue);
 	});
 	
 	$("#pgBase").on('keyup change', function() {
@@ -155,9 +278,10 @@ $(document).ready(function() {
 			from: $(this).val()
 		});
 		$vgBaseSlider.update({
-			from: 100 - $(this).val()
+			from: (100 - diluentBaseValue) - $(this).val()
 		});
-		$("#vgBase").val(100 - $(this).val());
+		$("#vgBase").val((100 - diluentBaseValue) - $(this).val());
 	});
+
 });
 
